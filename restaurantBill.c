f17ad820cf5f32610cb7370f13fef5f7c4da8e46
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-21 15:19:35 -0700

    First commit, comment outlines

---
+++ restaurantBill.c
@@ -0,0 +1,20 @@
+//headers go here, probably standard libraries
+
+
+
+//main goes here, add command line arguements for tax and tip
+
+//store meal costs given from assignment
+//salad,soup, sandwich, pizza.
+
+//calculate tax to be added to meal(meal is chosen randomly)
+
+//calculate tip to be added(tip is on pre-tax amount)
+
+//calculate total cost including everything
+
+
+//Display meal cost, tax amount, tip amount, and total bill
+
+//return success
+

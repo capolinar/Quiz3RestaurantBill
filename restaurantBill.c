097262d12fa77645604b1674e14104767b14c4fe
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-21 16:21:28 -0700

    added display messages for testing varaibles

--- restaurantBill.c
+++ restaurantBill.c
@@ -6,28 +6,39 @@
 //main goes here, add command line arguements for tax and tip
 int main(double argTax, double argTip ){ 
 
-
-//store meal costs given from assignment
-//salad,soup, sandwich, pizza.
-double salad = 9.95;
-double soup = 4.55
-double sandwich = 13.25;
-double pizza 22.35;
+    double tax = argTax;
+    double tip = argTip;
 
 
-
-//calculate tax to be added to meal(meal is chosen randomly)
-//also have to add random object
-srand((unsigned)  time(&t));
-
-//calculate tip to be added(tip is on pre-tax amount)
-
-//calculate total cost including everything
+    //store meal costs given from assignment
+    //salad,soup, sandwich, pizza.
+    double salad = 9.95;
+    double soup = 4.55;
+    double sandwich = 13.25;
+    double pizza = 22.35;
 
 
-//Display meal cost, tax amount, tip amount, and total bill
 
-//return success
+    //calculate tax to be added to meal(meal is chosen randomly)
+    //also have to add random variable
+    time_t  t;
+    srand((unsigned)  time(&t));
+    int rngFood = rand()  %  4 + 1; //random number between 1 and 4
+
+    //display message for test
+    printf("\ndisplaying arguement Tax: %f\n", tax);
+    printf("\ndislaying arguement Tip: %f\n", tip);
+    printf("\ndisplaying random number: %d\n", rngFood);
+
+
+    //calculate tip to be added(tip is on pre-tax amount)
+
+    //calculate total cost including everything
+
+
+    //Display meal cost, tax amount, tip amount, and total bill
+
+    //return success
     return EXIT_SUCCESS;
 }
 

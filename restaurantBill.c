760295b1148eaf123a802d4882ebf38930216325
Cody Apolinar <cnapolinar@cpp.edu>
2022-10-21 22:46:17 -0500

    Added calculations and printing code

--- restaurantBill.c
+++ restaurantBill.c
@@ -6,7 +6,7 @@
 //main goes here, add command line arguements for tax and tip
 int main(int argc, char *argv[] ){ 
 
-    printf("arguement count: %d\n", argc);
+    //printf("arguement count: %d\n", argc); //used to check that arguements passed correctly
 
     double tax;
     double tip;
@@ -28,19 +28,45 @@ int main(int argc, char *argv[] ){
     time_t  t;
     srand((unsigned)  time(&t));
     int rngFood = rand()  %  4 + 1; //random number between 1 and 4
+    double meal;
+    if(rngFood ==1)
+    {
+        meal = salad;
+    }
+    else if(rngFood == 2)
+    {
+        meal = soup;
+    }
+    else if(rngFood == 3)
+    {
+        meal = sandwich;
+    }
+    else
+    {
+        meal = pizza;
+    }
+    double cTax = meal * tax - meal;
 
     //display message for test
+    /* makes sure arguements and inputs are correct
     printf("\ndisplaying arguement Tax: %f\n", tax);
     printf("\ndislaying arguement Tip: %f\n", tip);
     printf("\ndisplaying random number: %d\n", rngFood);
+    */
 
 
     //calculate tip to be added(tip is on pre-tax amount)
+    double cTip = meal * tip - tip;
 
     //calculate total cost including everything
+    double total = meal + tax + tip;
 
 
     //Display meal cost, tax amount, tip amount, and total bill
+    printf("\nMeal cost: %.2f\n", meal);
+    printf("Tax: %.2f\n", tax);
+    printf("Tip: %.2f\n", tip);
+    printf("Total: %.2f\n", total);
 
     //return success
     return EXIT_SUCCESS;
